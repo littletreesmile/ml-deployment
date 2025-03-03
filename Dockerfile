252306587f23aa191cb34cc239393d0946c633b5
littletree <yelidk@gmail.com>
2025-03-03 13:48:04 +0800

    added Chinese to English translator app

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.11-bullseye
+
+WORKDIR /ml-deployment
+
+COPY . /ml-deployment
+
+RUN pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+RUN pip install -r requirements.txt
+
+CMD ["python", "app.py"]
+
---
+++ app.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, render_template, request
+from transformers import MarianMTModel, MarianTokenizer
+
+app = Flask(__name__)
+
+def translate(input_text):
+    model_name = "Helsinki-NLP/opus-mt-zh-en"
+    tokenizer = MarianTokenizer.from_pretrained(model_name)
+    model = MarianMTModel.from_pretrained(model_name)
+    translated = model.generate(**tokenizer(input_text, return_tensors="pt", padding=True))
+    res = [tokenizer.decode(t, skip_special_tokens=True) for t in translated]
+    return res[0] + "  -- xiaofat_LN"
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    translated_text = ""
+    input_text = ""
+    if request.method == "POST":
+        input_text = request.form["text"]
+        translated_text = translate(input_text)
+    return render_template("index.html", input_text=input_text, translated_text=translated_text)
+
+
+if __name__ == "__main__":
+    # app.run(debug=False, host="0.0.0.0", port=8080)  # for deployment
+    app.run(debug=False, port=8080)  # for local run
